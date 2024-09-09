e39ceabe016d90b8e27e544d182a06efd8f10a9b
javagenz <bonie@MacBook-Pro-Bonie.local>
2024-09-09 14:37:30 +0700

    update pertama

---
+++ Tutorpartone/BasicBagianSatu.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Tutorpartone
+{
+    class BasicBagianSatu
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program Start ===");  // Log Start.
+            Console.WriteLine(); // Mencetak baris kosong.
+            Console.WriteLine("Hello, World!"); // Mencetak string.
+            Console.WriteLine(42); // Mencetak integer.
+            Console.WriteLine(3.14); // Mencetak angka desimal.
+            Console.WriteLine("Hello, {0}!", "Amelia"); // Mencetak string yang diformat.
+            Console.WriteLine("=== Program End ==="); // Log End.
+        }
+    }
+}
